24339cf95fc53e806812e03079b5a2d64869998a
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2023-03-28 22:01:00 +0200

    some changes

--- run.bash
+++ run.bash
@@ -2,6 +2,6 @@
 
 . ./func/bash_funcs.bash
 
-print_heading "## 👢🧪(bootstrap): Home Lab Cloud ##" "=" 2
+print_heading "~~ 👢🧪(bootstrap): Home Lab Cloud ~~" "~" 2
 
-
+ansible-playbook -i inventory.yaml check_host.yaml
